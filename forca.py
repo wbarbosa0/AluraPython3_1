6f7056950269bd0b569be2fa19704b105e174f19
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-10-22 00:30:08 -0200

    Inicio de desenvolvimento da Forca

--- forca.py
+++ forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+    while(not acertou and not enforcou):
+        chute = input("Qual letra? ").strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper()==letra.upper()):
+                letras_acertadas[index]=chute
+            index=index+1
+        print(letras_acertadas)
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
