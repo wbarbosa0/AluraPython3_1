536bf1e333936c186c40d6f36365a7bd60942496
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-10-22 19:20:16 -0200

    Versão final, antes do refactoring

--- forca.py
+++ forca.py
@@ -1,28 +1,53 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    palavra_secreta = palavras[random.randrange(0,len(palavras))].upper()
+    #palavra_secreta = "banana".upper()
+    #letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = []
 
     print(letras_acertadas)
-    while(not acertou and not enforcou):
-        chute = input("Qual letra? ").strip()
+    while (not acertou and not enforcou):
+        chute = input("Qual letra? ").strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper()==letra.upper()):
-                letras_acertadas[index]=chute
-            index=index+1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = chute
+                index += 1
+            acertou = not "_" in letras_acertadas
+        else:
+            erros += chute
+            enforcou = len(erros) == 6
         print(letras_acertadas)
+        print("Erros: ", erros)
 
         print("Jogando...")
 
     print("Fim do jogo")
+    if (acertou):
+        print("Asertô, mizerávi!")
+    else:
+        print("Errôu! A palavra era {}", palavra_secreta)
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
